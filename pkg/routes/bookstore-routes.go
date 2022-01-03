3a40c2845184afb6df462dfeddcc67cfb3367597
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-03 23:27:11 +0900

    updated UpdateBook func

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,10 +1,13 @@
 package routes
 
+// user will hit when front-end is triggered
 import (
 	"github.com/YutaUtah/RESTAPIBook/pkg/controllers"
 	"github.com/gorilla/mux"
 )
 
+// for example, when someone hits /book/ is hit, trigger controllers.CreateBook is executed
+// https://pkg.go.dev/github.com/gorilla/mux@v1.8.0#Router.HandleFunc
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
