5caadfb6e73d7130ea5cc0f085be4091504c161d
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-03 23:23:53 +0900

    import directory typo fixed

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,13 @@ import (
 
 	"github.com/YutaUtah/RESTAPIBook/pkg/routes"
 	"github.com/gorilla/mux"
-	_ "github.com/zinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
+	// check this later
 	log.Fatal(http.ListenAndServe("localhost:9000", r))
 }
