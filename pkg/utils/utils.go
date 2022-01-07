c33863ce038c23fdbfe58eb862d2356ad53a8fab
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-07 17:54:00 +0900

    updated some comment in utils.go

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,3 +1,5 @@
+// this unmarshal JSON format data
+
 package utils
 
 import (
@@ -6,6 +8,7 @@ import (
 	"net/http"
 )
 
+// r is whatever we get from user. x is the interface
 func ParseBody(r *http.Request, x interface{}) {
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
