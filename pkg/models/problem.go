c0c98639e9f3233a881de146f4dfc4322e845bdd
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-07 20:34:46 +0900

    deleted the old file

---
+++ pkg/models/problem.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"github.com/YutaUtah/LeetCodeAPI/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Problem struct {
+	gorm.Model
+	Number     int    `gorm:""json:"number"`
+	Title      string `json:"title"`
+	Difficulty string `json:"difficulty"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	// automigrate empty problem
+	db.AutoMigrate(&Problem{})
+}
+
+// functions to communicate with database
+
+func (b *Problem) CreateProblem() *Problem {
+	// gorm function
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllProblems() []Problem {
+	// slice of Problem struct
+	var Problems []Problem
+	db.Find(&Problems)
+	return Problems
+}
+
+func GetProblemById(Id int64) (*Problem, *gorm.DB) {
+	var getProblem Problem
+	db := db.Where("ID=?", Id).Find(&getProblem)
+	return &getProblem, db
+}
+
+func DeleteProblem(ID int64) Problem {
+	var problem Problem
+	db.Where("ID=?", ID).Delete(problem)
+	return problem
+}
