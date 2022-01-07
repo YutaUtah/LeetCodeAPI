e5a47f22987b02be0c356f57bbb91ce65b20b2e5
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-07 17:52:22 +0900

    updated some comment in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -10,9 +10,12 @@ import (
 )
 
 func main() {
+	// initialize router
 	r := mux.NewRouter()
+	// function with multiple routing options
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	// check this later
+	// check this later help us create server in the first place
+	// if there is error print
 	log.Fatal(http.ListenAndServe("localhost:9000", r))
 }
