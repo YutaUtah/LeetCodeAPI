a2d44bee9d5868326e7677b574c31ad936c2c08b
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-07 20:29:30 +0900

    modified attribute from problemId to Id

---
+++ pkg/controllers/controller.go
@@ -0,0 +1,90 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/YutaUtah/LeetCodeAPI/pkg/models"
+	"github.com/YutaUtah/LeetCodeAPI/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+// var newProblems models.Problem
+
+func GetProblem(w http.ResponseWriter, r *http.Request) {
+	// store all problems info to newProblems and send it back to the user
+	newProblems := models.GetAllProblems()
+	// convert newProblems to json object
+	res, _ := json.Marshal(newProblems)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetProblemById(w http.ResponseWriter, r *http.Request) {
+	// get bars (json object)
+	vars := mux.Vars(r)
+	// get problemId by acccessing problemId attribute
+	problemId := vars["Id"]
+	ID, err := strconv.ParseInt(problemId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	problemDetails, _ := models.GetProblemById(ID)
+	res, _ := json.Marshal(problemDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateProblem(w http.ResponseWriter, r *http.Request) {
+	CreateProblem := &models.Problem{}
+	utils.ParseBody(r, CreateProblem)
+	b := CreateProblem.CreateProblem()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteProblem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	problemId := vars["Id"]
+	ID, err := strconv.ParseInt(problemId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	problem := models.DeleteProblem(ID)
+	res, _ := json.Marshal(problem)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateProblem(w http.ResponseWriter, r *http.Request) {
+	var updateProblem = &models.Problem{}
+	utils.ParseBody(r, updateProblem)
+	vars := mux.Vars(r)
+	problemId := vars["Id"]
+	ID, err := strconv.ParseInt(problemId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	problemDetails, db := models.GetProblemById(ID)
+	if number := strconv.Itoa(updateProblem.Number); number != "" {
+		problemDetails.Number = updateProblem.Number
+	}
+	if updateProblem.Title != "" {
+		problemDetails.Title = updateProblem.Title
+	}
+	if updateProblem.Difficulty != "" {
+		problemDetails.Difficulty = updateProblem.Difficulty
+	}
+	// saving problem details in database
+	db.Save(&problemDetails)
+	res, _ := json.Marshal(problemDetails)
+	w.Header().Set("Content-type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
