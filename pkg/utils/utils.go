1dd4b452467e78481a68a51bb1f015e4b03f200c
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-10 18:55:26 +0900

    added new error handler

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,6 +4,7 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -12,6 +13,7 @@ import (
 func ParseBody(r *http.Request, x interface{}) {
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
+			fmt.Println("error in unmarshalJSON")
 			return
 		}
 	}
