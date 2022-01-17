b9788ef52d6cd71ef0d42b6a731419e43309326e
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-01-17 23:43:59 +0900

    cleaned HTTP functions

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/YutaUtah/LeetCodeAPI/pkg/config"
 	"github.com/YutaUtah/LeetCodeAPI/pkg/routes"
-	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
 	// initialize router
 	var addr = flag.String("addr", ":9000", "endpoint address")
-	r := mux.NewRouter().StrictSlash(true)
+
+	app := config.App{}
+	v1 := app.GetSubrouter("/api/v1")
+
 	// function with multiple routing options
-	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/", r)
+	routes.RegisterLeetCodeRoutes(v1)
+	http.Handle("/", v1)
 	log.Println("Starting web server on", *addr)
-	log.Fatal(http.ListenAndServe("localhost:9000", r))
+	log.Fatal(http.ListenAndServe("localhost:9000", v1))
 }
